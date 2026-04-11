b9f93bf47b6b721c3653aa3a9f4a7b076f332df4
Luke Ferguson <lrfergus@ksu.edu>
2026-04-11 15:47:53 -0500

    Added css files to hold design elemets for pages and changed logos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,13 @@
 import '../App.css';
-//import logo from './logo.svg';
-import Members from '../components/Members';
-import Sponsors from '../components/Sponsors';
-import Photos from '../components/Photos.js';
 import Footer from '../components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import Logo from '../components/Logo.js';
-//import profile from '../img/default_profile.jpeg'
-//import mitch from '../img/mitchell_hutchinson_med.jpg'
-//import manny from '../img/manny_adeniji_med.jpg'
-import sicheng from '../img/sicheng_chen_med.png'
-//import laura from '../img/laura_legg_med.jpg'
-//import nathan from '../img/nathan_lee_med.png'
-//import jonah from '../img/jonah_bogusch_med.png'
-//import josh from '../img/weese_josh_med.jpg'
-//import cody from './img/cody_murphy_med.jpg'
-import sam from '../img/samuel_haynes_med.jpg'
-import taj from '../img/taj_ikhlaas_med.png'
-import emma from '../img/emma_lucas_med.jpg'
-import daniel from '../img/daniel_chang_med.png'
-//import supriya from '../img/supriya_med.jpeg'
-import pedro from '../img/pedro_med.jpg'
-//import tim from '../img/tim_med.jpeg'
-//import nathan from './img/nathan_york_med.png'
 import 'bootstrap/dist/css/bootstrap.css';
-//import boeing from '../img/boeing_logo.svg'
-//import betsol from '../img/betsol_logo.png'
-//import bugcrowd from '../img/bugcrowd_logo.svg'
-//import cboe from '../img/cboe_logo.svg'
-//import cerner from '../img/cerner_logo.png'
-//import civic from '../img/civicplus_logo.svg'
-//import conoco from '../img/conoco_logo.png'
-//import garmin from '../img/garmin_logo.svg'
-//import google from '../img/google_logo.svg'
-//import homebase from '../img/homebase_logo.svg'
-//import jnt from '../img/jnt_logo.png'
-//import koch from '../img/koch_logo.svg'
-import esports from '../img/esports_logo.png'
-//import netsmart from '../img/netsmart_logo.jpg'
-//import paycom from '../img/paycom_logo.png'
-//import tradebot from '../img/tradebot_logo.svg'
-//import softek from '../img/softek_logo.png'
-import { Nav, Navbar, Container } from "react-bootstrap";
-import { useRef, useState } from 'react';
+import { Container } from "react-bootstrap";
 import Navigation from "../components/Navigation.js"
 
 
-
 function Home() {
-  // const membersRef = useRef(null);
-  // const sponsorRef = useRef(null);
-
   return (
   <div className='App'>
     <Navigation/>
