f6b758093727ab9b753fe8cac5496a40deeb1188
Luke Ferguson <lrfergus@ksu.edu>
2026-04-20 16:37:25 -0500

    Modified the App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MeetUs from './pages/MeetUs';
-
 import EventsPage from './pages/EventsPage';
 import Sponsor from './pages/Sponsor';
-
 
 /* General Page to allow for different pages */
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/meet" element={<MeetUs />} />
-
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/sponsor" element={<Sponsor/>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/meet" element={<MeetUs />} />
+      <Route path="/events" element={<EventsPage />} />
+      <Route path="/sponsor" element={<Sponsor />} />
+    </Routes>
   );
 }
 
