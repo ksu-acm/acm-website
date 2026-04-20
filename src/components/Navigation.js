df90b11f9bba9ad8c0a210847696aa8b9eaf9eb5
Luke Ferguson <lrfergus@ksu.edu>
2026-04-20 16:44:08 -0500

    Modified Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,27 @@
 import '../App.css';
-//import logo from '../logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 function Navigation() {
-    return(
+    return (
         <Navbar className="navigation custom-nav" sticky="top" expand="lg" collapseOnSelect>
-        <Container>
-            <Navbar.Brand className="fw-bold">
-            <img src={new URL("https://upload.wikimedia.org/wikipedia/commons/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg")}
-             alt="logo" width="35px" height="35px" />{' '}
-            K-STATE ACM
-            </Navbar.Brand>
-            <Navbar.Toggle className="coloring" />
-            <Navbar.Collapse>
-            <Nav className="ms-auto fw-bold fs-6">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link href="/meet">Meet Us</Nav.Link>
-                <Nav.Link href="/sponsor">Sponsors</Nav.Link>
-                <Nav.Link as={Link} to="/events">Events</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
+            <Container>
+                <Navbar.Brand className="fw-bold">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg"
+                        alt="logo" width="35px" height="35px" />{' '}
+                    K-STATE ACM
+                </Navbar.Brand>
+                <Navbar.Toggle className="coloring" />
+                <Navbar.Collapse>
+                    <Nav className="ms-auto fw-bold fs-6">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/meet">Meet Us</Nav.Link>
+                        <Nav.Link as={Link} to="/sponsor">Sponsors</Nav.Link>
+                        <Nav.Link as={Link} to="/events">Events</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
